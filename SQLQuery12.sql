48f647de7cc061dc91c2d76000d68396466e4185
Eleonora Lombardo (c) <eleonora.lombardo@avanade.com>
2021-06-18 10:21:09 +0200

    Sql

---
+++ SQLQuery12.sql
@@ -0,0 +1,21 @@
+create database Studente_Esame
+
+create table Studente(
+id int identity(1,1) primary key,
+Nome varchar(40) not null,
+Cognome varchar(40) not null,
+AnnoNascita datetime not null)
+
+create table Esame(
+id int identity(1,1) primary key,
+Nome varchar(40) not null,
+CFU smallint not null,
+Datas datetime not null,
+idStudente int foreign key references Studente(id),
+Votazione smallint not null,
+Passato bit not null
+)
+
+insert into Studente(Nome, Cognome, AnnoNascita) values ('Pinco', 'Panco', 08-04-1995);
+insert into Studente(Nome, Cognome, AnnoNascita) values ('Cinco', 'DiMaggio', 08-04-1995);
+insert into Esame(Nome, CFU, Datas, idStudente, Votazione, Passato) values ('CryptoCurrencies', 6, 14-07-2021, 1, 30, 1);
